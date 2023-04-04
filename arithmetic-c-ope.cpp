31634f07ff3af7b2d837594f5a22e09c2cb6b208
pagidalamadeena <123723175+pagidalamadeena@users.noreply.github.com>
2023-04-04 09:24:48 +0530

    Add files via upload

---
+++ arithmetic-c-ope.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=5,b=4,res;
+	res=a+b;
+	printf("sum=%d\n",res);
+	res=a-b;
+	printf("substraction=%d\n",res);
+	res=a*b;
+	printf("multiply=%d\n",res);
+	res=a/b;
+	printf("divide=%d\n",res);
+	return 0;
+}
